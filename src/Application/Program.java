ac72f701c71c1053b5d989ac0c01a54995c5a85e
Valfrido Sales <valfridosales@hotmail.com>
2022-10-20 23:00:39 -0300

    att

--- src/Application/Program.java
+++ src/Application/Program.java
@@ -1,2 +1,47 @@
-package Application;public class Program {
+package Application;
+
+import Entities.Address;
+import Entities.Department;
+import Entities.Employee;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        Department dept = new Department();
+        System.out.print("Nome do departamento: ");
+        dept.setName(sc.nextLine());
+        System.out.print("Dia do pagamento: ");
+        dept.setPayday(sc.nextInt());
+        System.out.print("Email: ");
+        sc.nextLine();
+        String email = sc.nextLine();
+        System.out.print("Telefone: ");
+        sc.next();
+        String fone = sc.nextLine();
+        dept.setAddress(new Address(email, fone));
+
+        System.out.print("Quantos funcionários tem o departamento? ");
+        int N = sc.nextInt();
+
+        for (int i = 0; i < N; i++){
+            System.out.println("Dados do funcionário " + (i + 1) + ":");
+            System.out.print("Nome: ");
+            sc.nextLine();
+            String nome = sc.nextLine();
+            System.out.print("Salário: ");
+            double salario = sc.nextDouble();
+            dept.addEmployee(new Employee(nome, salario));
+        }
+
+        showReport(dept);
+        sc.close();
+    }
+    private static void showReport (Department dept){
+        System.out.println(dept.toString());
+    }
 }
--- src/Entities/Department.java
+++ src/Entities/Department.java
@@ -1,2 +1,88 @@
-package Entities;public class Department {
+package Entities;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Department {
+
+    private String name;
+
+    private Integer payday;
+
+    private Address address;
+
+    List <Employee> employeeList = new ArrayList<>();
+
+    public Department() {
+    }
+
+
+    public Department(String name, Integer payday, Address address) {
+        this.name = name;
+        this.payday = payday;
+        this.address = address;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getPayday() {
+        return payday;
+    }
+
+    public void setPayday(Integer payday) {
+        this.payday = payday;
+    }
+
+    public List<Employee> getEmployeeList() {
+        return employeeList;
+    }
+
+    public void setEmployeeList(List<Employee> employeeList) {
+        this.employeeList = employeeList;
+    }
+
+    public void addEmployee (Employee employee){
+            employeeList.add(employee);
+    }
+    public void removeEmployee (Employee employee){
+            employeeList.remove(employee);
+    }
+    public double payroll (){
+        double sum = 0.0;
+        for (int i = 0; i < employeeList.size(); i++){
+             sum += employeeList.get(i).getSalary();
+        }
+        return sum;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+           sb.append("FOLHA DE PAGAMENTO:" + "\n");
+           sb.append("Departamento ");
+           sb.append(getName()).append(" = R$ ");
+           sb.append(String.format("%.2f", payroll())).append("\n");
+           sb.append("Pagamento realizado no dia ").append(payday).append("\n");
+           sb.append("Funcionários:" + "\n");
+           for (Employee nome: employeeList){
+               sb.append(nome.getName()).append("\n");
+           }
+           sb.append("Para dúvidas favor entrar em contato: " + address.getEmail());
+
+         return sb.toString();
+    }
 }
